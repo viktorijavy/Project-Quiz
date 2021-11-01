5b55fc7fba1dd0b86ef5fc6c6e9f092c314de537
Viktorija Kuneviciute <viktorija.qun@gmail.com>
2021-11-01 22:16:34 +0100

    rearanged question data

--- app.js
+++ app.js
@@ -3,136 +3,118 @@
 let questions = [
     {
         question: "What is the best tv-show ever?",
-        answers: {
-          a: "game of thrones",
-          b: "friends",
-          c: "dark",
-          d: "breaking bad"
-        },
-        correctAnswer: "b"
+        answers: ["game of thrones", "friends", "dark","breaking bad"],
+        correctAnswer: 1
       },
       {
         question: "How much money were Friends cast paid per person/episode in the last seasons of the show?",
-        answers: {
-          a: "10 000",
-          b: "100 000",
-          c: "1 000  000",
-          d: "500 0000"
-        },
-        correctAnswer: "c"
+        answers: ["10 000", "100 000", "1 000 000","500 0000"],
+      
+        correctAnswer: 2
       },
       {
         question: "What is the best (imdb) rated tv show?",
-        answers: {
-          a: "Planet Earth II",
-          b: "Sopranos",
-          c: "The Wire",
-          d: "Game of Thrones"
-        },
-        correctAnswer: "a"
+        answers: ["Planet Earth II","Sopranos","The Wire","Game of Thrones"],
+        correctAnswer: 0
       },
       {
         question: "Which tv show has produced most seasons",
-        answers: {
-          a: "Guiding light",
-          b: "Days of our lives",
-          c: "Greys Anatomy",
-          d: "Emmerdale"
-        },
-        correctAnswer: "a"
+        answers: ["Guiding light", "Days of our lives","Greys Anatomy","Emmerdale"],
+        correctAnswer: 0
     },
     {
         question: "Where did the show 'The Wire' take place",
-        answers: {
-          a: "Chicago",
-          b: "LA",
-          c: "Detroit",
-          d: "Baltimore"
-        },
-        correctAnswer: "d"
+        answers: ["Chicago", "LA", "Detroit","Baltimore"],
+        correctAnswer: 3
     },
     {
         question: "Every episode of Seinfeld contains an image or reference to what superhero?",
-        answers: {
-          a: "Batman",
-          b: "Superman",
-          c: "Wonderwoman",
-          d: "Black Panther"
-        },
-        correctAnswer: "b"
+        answers: ["Batman","Superman","Wonderwoman","Black Panther"],
+        correctAnswer: 0
     },
     {
         question: "Where was 'Dark' filmed?",
-        answers: {
-          a: "in Winden",
-          b: "in Berlin/Brandenburg",
-          c: "in Bavaria",
-          d: "in Austria"
-        },
-        correctAnswer: "b"
+        answers: ["in Winden","in Berlin/Brandenburg","in Bavaria","in Austria"],
+        correctAnswer: 1
     },
     {
         question: "Which actor of The office was originaly director assistant before joining the cast?",
-        answers: {
-          a: "Jenna Fisher",
-          b: "Mindy Calling",
-          c: "Phyllis Smith",
-          d: "B.J. Novak"
-        },
-        correctAnswer: "c"
+        answers: ["Jenna Fisher","Mindy Calling","B.J. Novak","Phyllis Smith"],
+        correctAnswer: 3
     },
     {
         question: "What is the best tv show about vampires?",
-        answers: {
-          a: "What we do in the shadows",
-          b: "Dark Shadows",
-          c: "True Blood",
-          d: "The Vampire diaries"
-        },
-        correctAnswer: ""
+        answers: ["What we do in the shadows","Dark Shadows","True Blood","The Vampire diaries"],
+        correctAnswer: 0
     },
     {
         question: "Where did Breaking Bad take place?",
-        answers: {
-          a: "Alberqueque",
-          b: "Dallas",
-          c: "Santa Fe",
-          d: "Houston"
-        },
-        correctAnswer: ""
+        answers: ["Alberqueque","Dallas","Santa Fe","Houston"],
+        correctAnswer: 0
     }];
     //console.log(questions)
 
     const questionNumber = document.querySelector("#questionNr");
     const questionText = document.querySelector(".question-text");
     const answerBox = document.querySelector(".answer-box");
-
+    const nextButton = document.querySelector('.btn-next')
+    const startButton = document.querySelector('.start-button')
+    
+    
     let questionCounter = 0;
     let currentQuestion;
     let scoreCounter = 0;
+    let availableQuestions = [];
+    let availableAnswers = [];
 
+  
+    
+
+ function setAvailableQuestions() {
+     for (let i = 0; i < questions.length; i++){
+       availableQuestions.push(questions[i])
+     } 
+     return availableQuestions
+ } 
  
 
   
   function getNewQuestion() {
+      //getting question number to change at the top
       questionNumber.innerHTML = 'Question ' + (questionCounter + 1) + ' of ' + questions.length
+      //getting a random question displayed
       const randomQuestion = questions[Math.floor(Math.random()* questions.length)]
       currentQuestion = randomQuestion
       questionText.innerHTML = currentQuestion.question
-      questionCounter++
-  }
-  const nextButton = document.querySelector('.btn-next')
+      //getting possible answers in an array
+      const answerLength = currentQuestion.answers.length
+      for (let i = 0; i < answerLength; i++){
+          availableAnswers.push(i)
+      }
+      //displaying possible answers
+      for (let i=0; i < answerLength; i++) {
+          const answer = document.createElement("button")
+          answer.innerHTML = currentQuestion.answers[i];
+          answer.className = "answer";
+          answerBox.appendChild(answer)
+    }
+    questionCounter++
+}
+
+ 
+  
   nextButton.addEventListener('click', function() {
       if (questionCounter === questions.length) {
-          console.log('game over')
-      } else {
-          getNewQuestion()
-      }
-  })
-
-  window.onload = function() {
-      //setAvailableQuestions()
-      getNewQuestion()
-  }
+          displayResults()
+     } else {
+         getNewQuestion()
+     }
+ })
+   
+  
+window.onload = function() {
+    getNewQuestion()
+}
+  
   
 
